8f70d23db6166cd53d659079882bc6574057de29
al3xar <al3xar@izertis.com>
2025-01-17 19:50:23 +0100

    feat: securización activada

--- wilma-config/config.js
+++ wilma-config/config.js
@@ -2,42 +2,42 @@
 const config = {};
 
 // Used only if https is disabled
-config.pep_port = 3003;
+config.pep_port = 1020;
 
 // Set this var to undefined if you don't want the server to listen on HTTPS
 config.https = {
   enabled: true,
-  cert_file: './certs/cert.crt',
-  key_file: './certs/key.key',
-  port: 443
+  cert_file: "./certs/cert.crt",
+  key_file: "./certs/key.key",
+  port: 1026,
 };
 
 config.idm = {
-  host: 'localhost',
-  port: 3000,
-  ssl: false
+  host: "10.0.2.15",
+  port: 3443,
+  ssl: true,
 };
 
 config.app = {
-  host: 'localhost',
-  port: '3002',
-  ssl: false // Use true if the app server listens in https
+  host: "10.0.2.15",
+  port: 1028,
+  ssl: true, // Use true if the app server listens in https
 };
 
 config.organizations = {
   enabled: false,
-  header: 'fiware-service'
+  header: "fiware-service",
 };
 
 // Credentials obtained when registering PEP Proxy in app_id in Account Portal
 config.pep = {
-  app_id: 'd6ef7f8c-7c70-4b59-8e3d-121bcd316d47',
-  username: 'pep_proxy_f196e6c7-4584-4918-88d3-d5ea0aeac36c',
-  password: 'pep_proxy_114a7dfc-f694-4800-bd42-a350600e629c',
+  app_id: "d6ef7f8c-7c70-4b59-8e3d-121bcd316d47",
+  username: "pep_proxy_f196e6c7-4584-4918-88d3-d5ea0aeac36c",
+  password: "pep_proxy_114a7dfc-f694-4800-bd42-a350600e629c",
   token: {
-    secret: '' // Secret must be configured in order validate a jwt
+    secret: "", // Secret must be configured in order validate a jwt
   },
-  trusted_apps: []
+  trusted_apps: [],
 };
 
 // in seconds
@@ -53,17 +53,17 @@ config.cache_time = 300;
 
 config.authorization = {
   enabled: true,
-  pdp: 'idm', // idm|iShare|xacml|authzforce
-  header: undefined, // NGSILD-Tenant|fiware-service
+  pdp: "idm", // idm|iShare|xacml|authzforce
+  header: "fiware-service", // NGSILD-Tenant|fiware-service
   location: {
-    protocol: 'http',
-    host: 'localhost',
-    port: 8080,
-    path: ''
+    protocol: "https",
+    host: "10.0.2.15",
+    port: 3443,
+    path: "",
   },
   azf: {
-    custom_policy: undefined // use undefined to default policy checks (HTTP verb + path).
-  }
+    custom_policy: undefined, // use undefined to default policy checks (HTTP verb + path).
+  },
 };
 
 config.cors = {
